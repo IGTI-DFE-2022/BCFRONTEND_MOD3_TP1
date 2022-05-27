9f854417d734e64ec9029bc31089578038e77f93
Fernando Ferreira <fferreira12@gmail.com>
2022-05-27 13:10:46 -0300

    App made

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Conversor from "./components/Conversor.vue";
+</script>
+
+<template>
+  <div class="container">
+    <div class="header">Reais Para Dolares</div>
+    <Conversor></Conversor>
+  </div>
+</template>
+
+<style>
+@import "assets/base.css";
+
+.container {
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+}
+
+.header {
+  line-height: 1.5;
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+}
+</style>
---
+++ src/components/Conversor.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { reactive } from "vue";
+
+const state = reactive({ exchangeFactor: 5, valueInput: 100 });
+
+function calculate() {
+  return Math.round((100 * state.valueInput) / state.exchangeFactor) / 100;
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="input">
+      <input
+        type="number"
+        placeholder="Valor em Reais"
+        v-model="state.valueInput"
+      />
+    </div>
+    <div class="input">
+      <input
+        type="number"
+        placeholder="Fator de Conversão"
+        v-model="state.exchangeFactor"
+      />
+    </div>
+
+    <div class="result">US$ {{ calculate().toFixed(2) }}</div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+}
+
+input {
+  padding: 1rem;
+  margin: 0 0 1rem;
+  font-size: 1.5rem;
+  text-align: center;
+}
+
+.result {
+  font-size: 2rem;
+}
+</style>
